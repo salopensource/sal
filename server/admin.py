f48c9b357b0b319d4c70e303c62352fda2c026c4
Graham Gilbert <graham@grahamgilbert.com>
2013-09-11 10:19:49 +0100

    Loads of changes

--- server/admin.py
+++ server/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from server.models import *
     
+class BusinessUnitAdmin(admin.ModelAdmin):
+    readonly_fields=('key',)
 admin.site.register(UserProfile)
-admin.site.register(BusinessUnit)
+admin.site.register(BusinessUnit, BusinessUnitAdmin)
 admin.site.register(MachineGroup)
 admin.site.register(Machine)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+import random
+import string
 
+# def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+# ...    return ''.join(random.choice(chars) for x in range(size))
+def GenerateKey():
+    key = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(128))
+    try:
+        BusinessUnit.objects.get(key=key)
+        return GenerateKey()
+    except BusinessUnit.DoesNotExist:
+        return key;
 class UserProfile(models.Model):
     user = models.OneToOneField(User, unique=True)
 
@@ -11,17 +22,27 @@ class UserProfile(models.Model):
         ('GA', 'Global Admin'),
     )
     level = models.CharField(max_length=2, choices=LEVEL_CHOICES, default='SO')
-User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+User.userprofile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
 class BusinessUnit(models.Model):
     name = models.CharField(max_length=100)
-    key = models.CharField(max_length=256)
+    key = models.CharField(max_length=256, unique=True, blank=True, null=True, editable=False)
     users = models.ManyToManyField(User)
     
+    def save(self):
+            if not self.id:
+                self.key = GenerateKey()
+            super(BusinessUnit, self).save()
+            
+    def __unicode__(self):
+        return self.name
+
 class MachineGroup(models.Model):
     business_unit = models.ForeignKey(BusinessUnit)
     name = models.CharField(max_length=100)
     manifest = models.CharField(max_length=256)
+    def __unicode__(self):
+        return self.name
     
 class Machine(models.Model):
     serial = models.CharField(max_length=100)
@@ -32,3 +53,5 @@ class Machine(models.Model):
     manifest = models.CharField(max_length=256)
     hd_space = models.CharField(max_length=256)
     last_checkin = models.DateTimeField(blank=True,null=True)
+    def __unicode__(self):
+        return self.hostname

0961099747a876bdd2bafd88946ddecf81edf06c
Graham Gilbert <graham@grahamgilbert.com>
2014-02-19 09:09:23 +0000

    Show Serial number in admin

--- server/admin.py
+++ server/admin.py
@@ -3,10 +3,13 @@ from server.models import *
     
 class MachineGroupAdmin(admin.ModelAdmin):
     readonly_fields=('key',)
+
+class MachineAdmin(admin.ModelAdmin):
+    list_display = ('hostname', 'serial')
 admin.site.register(UserProfile)
 admin.site.register(BusinessUnit)
 admin.site.register(MachineGroup, MachineGroupAdmin)
-admin.site.register(Machine)
+admin.site.register(Machine, MachineAdmin)
 admin.site.register(Fact)
 admin.site.register(Condition)
 admin.site.register(PendingUpdate)

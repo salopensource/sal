6bb7de929d62d0fe7ec8ad252f2d281436e1bcd6
Graham Gilbert <graham@grahamgilbert.com>
2014-05-14 10:24:33 +0100

    Potential fix for issues #16 and #24

--- server/templatetags/dashboard_extras.py
+++ server/templatetags/dashboard_extras.py
@@ -2,6 +2,7 @@ from django import template
 from django.shortcuts import get_object_or_404
 from server.models import *
 from datetime import datetime
+import dateutil.parser
 from django.template.defaultfilters import date
 
 register = template.Library()
@@ -13,7 +14,7 @@ def humanreadablesize(kbytes):
         kbytes = float(kbytes)
     except (TypeError, ValueError, UnicodeDecodeError):
         return "unknown"
-        
+
     units = [(" KB", 2**10), (" MB", 2**20), (" GB", 2**30), (" TB", 2**40)]
     for suffix, limit in units:
         if kbytes > limit:
@@ -31,13 +32,14 @@ def bu_machine_count(bu_id):
     machine_groups = business_unit.machinegroup_set.all()
     count = 0
     for machinegroup in machine_groups:
-        count = count + machinegroup.machine_set.count()    
+        count = count + machinegroup.machine_set.count()
     return count
 
 @register.filter
 def convert_datetime(string):
     """Converts a string into a formatted date"""
-    the_date = datetime.strptime(string, "%Y-%m-%d %H:%M:%S +0000")
+    the_date = dateutil.parser.parse(string)
+    print the_date
     return date(the_date, "Y-m-d H:i")
 
 @register.filter

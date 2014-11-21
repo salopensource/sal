d9d6906ca57ad6dc9f9fb664a7961a01abe33438
Graham Gilbert <graham@grahamgilbert.com>
2014-11-21 09:43:07 +0000

    Tick boxes when adding users to a BU is nicer

--- server/forms.py
+++ server/forms.py
@@ -4,7 +4,10 @@ from models import *
 class BusinessUnitForm(forms.ModelForm):
     class Meta:
         model = BusinessUnit
-        # fields = ('name','prefix','domain',)
+        widgets = {'users': forms.CheckboxSelectMultiple}
+    def __init__(self, *args, **kwargs):
+        super(BusinessUnitForm, self).__init__(*args, **kwargs)
+        self.fields['users'].help_text = ''
         
 class MachineGroupForm(forms.ModelForm):
     class Meta:
@@ -15,3 +18,17 @@ class EditMachineGroupForm(forms.ModelForm):
     class Meta:
         model = MachineGroup
         fields = ('name',)
+
+# class UserToBU(forms.ModelForm):
+#     bus = forms.ModelMultipleChoiceField(
+#         queryset=Entry.objects.all(),
+#         widget=forms.CheckboxSelectMultiple)
+
+#     class Meta:
+#         model = BusinessUnit
+
+#     def __init__(self, *args, **kwargs):
+#         super(BlogForm, self).__init__(*args, **kwargs)
+#         if self.instance:
+#             entries = Entry.objects.filter(blog=blog)
+#             self.fields['entries'].queryset = entries

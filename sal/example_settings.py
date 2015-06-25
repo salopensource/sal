dd5aa65b07f67718086afbc5c213183cafce4f11
Graham Gilbert <graham@grahamgilbert.com>
2015-06-25 12:08:21 +0100

    Modified example_settings

---
+++ sal/example_settings.py
@@ -0,0 +1,14 @@
+# Django settings for sal project.
+from system_settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_DIR, 'sal.db'),                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}

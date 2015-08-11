759f6a2e4ced9ce9beeda01e638f109d946050b1
Graham Gilbert <graham@grahamgilbert.com>
2015-08-11 08:26:34 +0100

    Add in the first checkin date if it doesn't exist

--- server/migrations/0006_auto_20150811_0811.py
+++ server/migrations/0006_auto_20150811_0811.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.shortcuts import get_object_or_404
 from django.db import models, migrations
 
+def add_initial_date(apps, schema_editor):
+    Machine = apps.get_model("server", "Machine")
+    for machine in Machine.objects.all():
+        if not machine.first_checkin:
+            machine.first_checkin = machine.last_checkin
+            machine.save()
 
 class Migration(migrations.Migration):
 
@@ -21,4 +27,5 @@ class Migration(migrations.Migration):
             name='sal_version',
             field=models.TextField(null=True, blank=True),
         ),
+        migrations.RunPython(add_initial_date),
     ]

800811499bdddf56fe75adc695787daf39a4b0dd
Graham Gilbert <graham@grahamgilbert.com>
2015-09-16 18:31:05 +0100

    Nginx error log to stdout

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -1,7 +1,7 @@
 import multiprocessing
 from os import getenv
 bind = '127.0.0.1:8001'
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() * 4 + 1
 # Read the DEBUG setting from env var
 try:
     if getenv('DOCKER_SAL_DEBUG').lower() == 'true':

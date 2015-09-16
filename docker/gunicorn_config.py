dd4190d6c6a2c8b76b97bcad12c6ef224ce02a51
Graham Gilbert <graham@grahamgilbert.com>
2015-09-16 18:13:57 +0100

    Error logs to stdout when in debug mode

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -5,7 +5,10 @@ workers = multiprocessing.cpu_count() * 2 + 1
 # Read the DEBUG setting from env var
 try:
     if getenv('DOCKER_SAL_DEBUG').lower() == 'true':
-        errorlog = '/var/log/nginx/gunicorn-error.log'
-        accesslog = '/var/log/nginx/gunicorn-access.log'
+        # errorlog = '/var/log/nginx/gunicorn-error.log'
+        # accesslog = '/var/log/nginx/gunicorn-access.log'
+        errorlog = '-'
+        loglevel = 'info'
+        accesslog = '-'
 except:
     pass

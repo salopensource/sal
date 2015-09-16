bcbca018f363e22b482293f46c46cf40a396b012
Graham Gilbert <graham@grahamgilbert.com>
2015-09-16 19:05:32 +0100

    Tail the log files

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -5,10 +5,8 @@ workers = multiprocessing.cpu_count() * 4 + 1
 # Read the DEBUG setting from env var
 try:
     if getenv('DOCKER_SAL_DEBUG').lower() == 'true':
-        # errorlog = '/var/log/nginx/gunicorn-error.log'
-        # accesslog = '/var/log/nginx/gunicorn-access.log'
-        errorlog = '-'
+        errorlog = '/var/log/gunicorn/gunicorn-error.log'
+        accesslog = '/var/log/gunicorn/gunicorn-access.log'
         loglevel = 'info'
-        accesslog = '-'
 except:
     pass
--- docker/run.sh
+++ docker/run.sh
@@ -15,6 +15,9 @@ fi
 chown -R www-data:www-data $APP_DIR
 chmod go+x $APP_DIR
 mkdir -p /var/log/gunicorn
+touch /var/log/gunicorn/gunicorn-error.log
+/var/log/gunicorn/gunicorn-access.log
+tail -n 0 -f /var/logs/gunicorn*.log &
 export PYTHONPATH=$PYTHONPATH:$APP_DIR
 export DJANGO_SETTINGS_MODULE='sal.settings'
 #export SECRET_KEY='no-so-secret' # Fix for your own site!

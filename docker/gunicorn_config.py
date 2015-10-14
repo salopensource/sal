cbf862a6ce55c00da5d478a80467c5ccd48c48bc
Graham Gilbert <graham@grahamgilbert.com>
2015-10-14 12:40:13 +0100

    This is too low

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -4,7 +4,7 @@ bind = '127.0.0.1:8001'
 workers = multiprocessing.cpu_count() * 2
 timeout = 60
 threads = multiprocessing.cpu_count() * 2
-max_requests = 100
+max_requests = 1000
 max_requests_jitter = 5
 # Read the DEBUG setting from env var
 try:

01331a7ecbb5ec93943cfa57ca961e22921bd766
Graham Gilbert <graham@grahamgilbert.com>
2015-10-14 12:08:45 +0100

    Protect against high usage eating all memory

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -4,6 +4,7 @@ bind = '127.0.0.1:8001'
 workers = multiprocessing.cpu_count() * 2
 timeout = 60
 threads = multiprocessing.cpu_count() * 2
+max_requests = 100
 # Read the DEBUG setting from env var
 try:
     if getenv('DOCKER_SAL_DEBUG').lower() == 'true':

fb17297d9d4717149f71c8f8e6542596dd93407a
Graham Gilbert <graham@grahamgilbert.com>
2016-01-03 16:02:21 +0000

    Maths is hard

--- server/utils.py
+++ server/utils.py
@@ -24,10 +24,8 @@ def get_version_number():
         last_sent = SalSetting(name='last_sent_data', value='0')
         last_sent.save()
 
-    current_time = time.time()
-    if int(float(current_time - 86400)) < int(float(last_sent.value)) or int(last_sent.value) == 0:
-        print int(float(last_sent.value))
-        print int(float(current_time - 86400))
+    current_time = int(time.time())
+    if (current_time - 86400) > last_sent.value or last_sent.value == 0:
         try:
             current_version = SalSetting.objects.get(name='current_version')
         except SalSetting.DoesNotExist:

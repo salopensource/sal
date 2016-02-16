ea225d050ad85501809e8aeeb2c8c31557e622cb
Shea Craig <sheagcraig@gmail.com>
2016-02-16 12:55:10 -0500

    Update inventory models.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from inventory.models import Inventory, InventoryItem
 
+
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bundleid', 'bundlename')
+
+
 class InventoryAdmin(admin.ModelAdmin):
     list_display = ('machine', 'datestamp', 'sha256hash')
 
+
 class InventoryItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'version', 'path')
+    list_display = ('get_name', 'version', 'path')
+
+    def get_name(self, item):
+        return item.application.name
+
 
 admin.site.register(Inventory, InventoryAdmin)
 admin.site.register(InventoryItem, InventoryItemAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -1,24 +1,33 @@
 from django.db import models
 from server.models import *
 import watson
-# Create your models here.
+
+
+class Application(models.Model):
+    name = models.CharField(db_index=True, max_length=255)
+    bundleid = models.CharField(db_index=True, max_length=255)
+    bundlename = models.CharField(db_index=True, max_length=255)
+
+    class Meta:
+        ordering = ['name']
+
 
 class Inventory(models.Model):
     machine = models.ForeignKey(Machine)
     datestamp = models.DateTimeField(auto_now=True)
     sha256hash = models.CharField(max_length=64)
+
     class Meta:
         ordering = ['datestamp']
 
 
 class InventoryItem(models.Model):
     machine = models.ForeignKey(Machine)
-    name = models.CharField(db_index=True, max_length=255)
+    application = models.ForeignKey(Application)
     version = models.CharField(db_index=True, max_length=32)
-    bundleid = models.CharField(db_index=True, max_length=255)
-    bundlename = models.CharField(db_index=True, max_length=255)
     path = models.TextField()
-    class Meta:
-        ordering = ['name', '-version']
+
+    # class Meta:
+    #     ordering = ['name', '-version']
 
 watson.register(InventoryItem)

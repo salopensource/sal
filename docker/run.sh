7865f769cecc8af57ee3d09353dcf193724ab1dd
Graham Gilbert <graham@grahamgilbert.com>
2016-02-18 07:46:24 +0000

    More conversion from data tables, echo out sal's log for docker

--- docker/run.sh
+++ docker/run.sh
@@ -23,7 +23,9 @@ chmod go+x $APP_DIR
 mkdir -p /var/log/gunicorn
 touch /var/log/gunicorn/gunicorn-error.log
 touch /var/log/gunicorn/gunicorn-access.log
-tail -n 0 -f /var/log/gunicorn/gunicorn*.log &
+touch $APP_DIR/sal.log
+chmod 777 $APP_DIR/sal.log
+tail -n 0 -f /var/log/gunicorn/gunicorn*.log & tail -n 0 -f $APP_DIR/sal.log &
 export PYTHONPATH=$PYTHONPATH:$APP_DIR
 export DJANGO_SETTINGS_MODULE='sal.settings'
 #export SECRET_KEY='no-so-secret' # Fix for your own site!

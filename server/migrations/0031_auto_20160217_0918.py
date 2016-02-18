906892064fc94f466613c6066fe6ab4f37b3441f
Graham Gilbert <graham@grahamgilbert.com>
2016-02-18 10:48:03 +0000

    Give a hostname to those not fortunate enough to have one

--- server/migrations/0031_auto_20160217_0918.py
+++ server/migrations/0031_auto_20160217_0918.py
@@ -10,6 +10,14 @@ def fix_hostname(apps, schema_editor):
     Machine = apps.get_model("server", "Machine")
 
     machines = Machine.objects.filter(hostname__isnull=True)
+    for machine in machines:
+        machine.hostname = machine.serial
+        machine.save()
+
+    machines = Machine.objects.filter(hostname__exact='')
+    for machine in machines:
+        machine.hostname = machine.serial
+        machine.save()
 
 class Migration(migrations.Migration):
 

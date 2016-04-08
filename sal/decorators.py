4d00bc00fec8217a3d7826c2019ab2bfdb230235
Shea Craig <sheagcraig@gmail.com>
2016-04-07 21:23:03 -0400

    Refactor access decorator.

--- sal/decorators.py
+++ sal/decorators.py
@@ -20,33 +20,40 @@ def class_login_required(cls):
 
 
 def class_access_required(cls):
-    """Decorator for View subclasses to restrict by business unit."""
-    if not isinstance(cls, type) or not issubclass(cls, View):
-        raise Exception("Must be applied to subclass of View")
+    """Decorator for View subclasses to restrict by business unit.
 
+    Class must declare a classmethod `get_business_unit` that returns
+    the BusinessUnit object that applies to the query in question.
+
+    Args:
+        cls: Class to decorate.
+
+    Returns:
+        Decorated class.
+
+    Raises:
+        403 Pemission Denied if current user does not have access.
+    """
     def access_required(f):
         def decorator(*args, **kwargs):
             user = args[0].user
-            if "bu_id" in kwargs:
-                business_unit = get_object_or_404(
-                    BusinessUnit, pk=kwargs['bu_id'])
-            elif "group_id" in kwargs:
-                business_unit = get_object_or_404(
-                    MachineGroup, pk=kwargs["group_id"]).business_unit
-            elif "machine_id" in kwargs:
-                business_unit = get_object_or_404(
-                    Machine,
-                    pk=kwargs["machine_id"]).machine_group.business_unit
-            else:
-                business_unit = None
+            business_unit = cls.get_business_unit(**kwargs)
 
-            if (not user.userprofile.level == "GA" and (not business_unit or
-                business_unit not in user.businessunit_set.all())):
-                raise PermissionDenied()
-            else:
+            if is_global_admin(user) or has_access(user, business_unit):
                 return f(*args, **kwargs)
+            else:
+                raise PermissionDenied()
         return decorator
 
     access_decorator = method_decorator(access_required)
     cls.dispatch = access_decorator(cls.dispatch)
     return cls
+
+
+def is_global_admin(user):
+    return user.userprofile.level == "GA"
+
+
+def has_access(user, business_unit):
+    return (business_unit and
+            business_unit in user.businessunit_set.all())

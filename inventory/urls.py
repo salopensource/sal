7477805dcd1f020d265496eb7a5ccc81147ba007
Shea Craig <sheagcraig@gmail.com>
2016-04-07 21:25:14 -0400

    Rewrite inventory as GCBVs. Overhaul urls and templates.

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
 from inventory import views
 
 urlpatterns = [
@@ -11,41 +10,22 @@ urlpatterns = [
     # CSV Export (id)
     url(r'^id_csv/(?P<page>.+)/(?P<theID>.+)/$', views.export_csv,
         name='inventory_export_csv_id'),
-    # GA Application Detail
-    url(r'^application/(?P<pk>[0-9]+)/$',
-        views.ApplicationDetailView.as_view(),
-        name="application-detail"),
-    # Business Unit Application Detail
-    url(r'^business_unit/(?P<bu_id>[0-9]+)/application/(?P<pk>[0-9]+)/$',
-        views.ApplicationDetailView.as_view(),
-        name="bu-application-detail"),
-    # Machine Group Application Detail
-    url(r'^machine_group/(?P<group_id>[0-9]+)/'
-        r'application/(?P<pk>[0-9]+)/$',
-        views.ApplicationDetailView.as_view(),
-        name="mg-application-detail"),
-    # Machine Group Application Detail
-    url(r'^machine/(?P<machine_id>[0-9]+)/'
-        r'application/(?P<pk>[0-9]+)/$',
-        views.ApplicationDetailView.as_view(),
-        name="machine-application-detail"),
+    # Application Detail View
+     url(r'^application/'
+         r'(?P<pk>[0-9]+)/'
+         r'(?P<group_type>[a-zA-Z_-]+)/'
+         r'(?P<group_id>[0-9]+)/$',
+         views.ApplicationDetailView.as_view(),
+         name="application-detail"),
+    # Install List View
+    url(r'^list/(?P<group_type>.+)/'
+        r'(?P<group_id>[0-9]+)/'
+        r'(?P<application_id>[0-9]+)/'
+        r'(?P<field_type>[a-zA-Z]+)/{1}'
+        r'(?P<field_value>.+)/$',
+        views.InventoryListView.as_view(), name='inventory_list'),
     # GA Application List
-    url(r'^$', views.ApplicationListView.as_view(),
-        name="application-list"),
-    # Business Unit Application List
-    url(r'^business_unit/(?P<bu_id>.+)/$',
-        views.BusinessUnitApplicationListView.as_view(),
-        name="bu_inventory"),
-    # Machine Group Application List
-    url(r'^machine_group/(?P<group_id>.+)/$',
-        views.MachineGroupApplicationListView.as_view(),
-        name="machine_group_inventory"),
-    # Machine Application List
-    url(r'^machine/(?P<machine_id>.+)/$',
-        views.MachineApplicationListView.as_view(), name="machine_inventory"),
-    # Inventory List (front page)
-    # url(r'^list/$', views.inventory_list, name='inventory_list_front'),
-    # Inventory List (id)
-    # url(r'^id_list/(?P<page>.+)/(?P<theID>.+)/$', views.inventory_list,
-    #     name='inventory_list_id'),
+    url(r'^(?P<group_type>.+)/'
+        r'(?P<group_id>[0-9]+)/$', views.ApplicationListView.as_view(),
+        name="application_list"),
 ]

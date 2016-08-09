b72615aa7b2d4d31bfe6beaacfaeae6f6827411c
Graham Gilbert <graham@grahamgilbert.com>
2016-08-09 15:11:22 +0100

    Don't operate on EVERYTHING

--- server/views.py
+++ server/views.py
@@ -699,11 +699,17 @@ def export_csv(request, pluginName, data, page='front', theID=None):
     for field in fields:
         if not field.is_relation and field.name != 'id' and field.name != 'report' and field.name != 'activity' and field.name != 'os_family' and field.name != 'install_log' and field.name != 'install_log_hash':
             header_row.append(field.name)
-    distinct_facts = Fact.objects.all().values('fact_name').distinct()
+    distinct_facts = Fact.objects.all().values('fact_name').distinct().order_by('fact_name')
+
+    facter_headers = []
     for distinct_fact in distinct_facts:
+        facter_headers.append('Facter: '+ distinct_fact['fact_name'])
         header_row.append('Facter: '+ distinct_fact['fact_name'])
-    distinct_conditions = Condition.objects.all().values('condition_name').distinct()
+    distinct_conditions = Condition.objects.all().values('condition_name').distinct().order_by('condition_name')
+
+    condition_headers = []
     for distinct_condition in distinct_conditions:
+        condition_headers.append('Munki Condition: '+ distinct_condition['condition_name'])
         header_row.append('Munki Condition: '+ distinct_condition['condition_name'])
     header_row.append('business_unit')
     header_row.append('machine_group')
@@ -715,12 +721,12 @@ def export_csv(request, pluginName, data, page='front', theID=None):
             if name != 'id' and name !='machine_group' and name != 'report' and name != 'activity' and name != 'os_family' and name != 'install_log' and name != 'install_log_hash':
                 row.append(value.strip())
 
-        facts = machine.facts.all().values('fact_name', 'fact_data')
-        for header_item in header_row:
+        facts = machine.facts.all().values('fact_name', 'fact_data').order_by('fact_name')
+        for header_item in facter_headers:
             row.append(utils.csvrelated(header_item, facts, 'facter'))
 
-        conditions = machine.conditions.all().values('condition_name', 'condition_data')
-        for header_item in header_row:
+        conditions = machine.conditions.all().values('condition_name', 'condition_data').order_by('condition_name')
+        for header_item in condition_headers:
             row.append(utils.csvrelated(header_item, conditions, 'condition'))
         row.append(machine.machine_group.business_unit.name)
         row.append(machine.machine_group.name)

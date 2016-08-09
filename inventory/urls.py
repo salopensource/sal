9ab0c3dd97d22a8565070353ed6a3c05542aca18
Graham Gilbert <graham@grahamgilbert.com>
2016-08-09 11:49:34 +0100

    Nicer name for view

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     url(r'^list/$', views.inventory_list, name='inventory_list_front'),
     # Inventory List (id)
     url(r'^id_list/(?P<page>.+)/(?P<theID>.+)/$', views.inventory_list, name='inventory_list_id'),
-    url(r'^hash/(?P<serial>.+)/$', views.inventory_hash),
-    url(r'^business_unit/(?P<bu_id>.+)/$', views.bu_inventory),
-    url(r'^machine_group/(?P<group_id>.+)/$', views.machine_group_inventory),
-    url(r'^machine/(?P<machine_id>.+)/$', views.machine_inventory),
+    url(r'^hash/(?P<serial>.+)/$', views.inventory_hash name='inventory_hash'),
+    url(r'^business_unit/(?P<bu_id>.+)/$', views.bu_inventory, name='bu_inventory'),
+    url(r'^machine_group/(?P<group_id>.+)/$', views.machine_group_inventory, name='machine_group_inventory'),
+    url(r'^machine/(?P<machine_id>.+)/$', views.machine_inventory, name='machine_inventory'),
     # CSV Export (front page)
     url(r'^csv/$', views.export_csv, name='inventory_export_csv_front'),
     # CSS Ecport (id)

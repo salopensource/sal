906747292fc732abe11685ea86e1515e87940038
Graham Gilbert <graham@grahamgilbert.com>
2016-09-09 19:06:01 +0100

    Critical errors

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -10,7 +10,7 @@ threads = multiprocessing.cpu_count() * 2
 pidfile = '/var/run/gunicorn.pid'
 # max_requests_jitter = 50
 errorlog = '/var/log/gunicorn/gunicorn-error.log'
-loglevel = 'error'
+loglevel = 'critical'
 # Read the DEBUG setting from env var
 try:
     if getenv('DOCKER_SAL_DEBUG').lower() == 'true':

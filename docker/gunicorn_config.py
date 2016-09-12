228c7d4674303a226315236650c49a91835596d6
Graham Gilbert <graham@grahamgilbert.com>
2016-09-12 19:58:47 +0100

    Max requests is back

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -6,9 +6,9 @@ workers = multiprocessing.cpu_count() * 2 + 1
 graceful_timeout = 120
 timeout = 90
 threads = multiprocessing.cpu_count() * 2
-# max_requests = 600
+max_requests = 600
 pidfile = '/var/run/gunicorn.pid'
-# max_requests_jitter = 50
+max_requests_jitter = 50
 errorlog = '/var/log/gunicorn/gunicorn-error.log'
 loglevel = 'critical'
 # Read the DEBUG setting from env var

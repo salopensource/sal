4f8d72145851b5a261487b3b4e914a88d8ead8c5
Graham Gilbert <graham@grahamgilbert.com>
2016-09-12 05:57:36 +0100

    Higher timeouts to allow for large exports

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -3,8 +3,8 @@ from os import getenv
 bind = '127.0.0.1:8001'
 
 workers = multiprocessing.cpu_count() * 2 + 1
-graceful_timeout = 60
-timeout = 30
+graceful_timeout = 120
+timeout = 90
 threads = multiprocessing.cpu_count() * 2
 # max_requests = 600
 pidfile = '/var/run/gunicorn.pid'

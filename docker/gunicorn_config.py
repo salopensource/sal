1308595361e39a77dc1ee142a70db711ec301ef0
Graham Gilbert <graham@grahamgilbert.com>
2016-09-14 18:15:52 +0100

    Try some more of the defaults

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -3,10 +3,10 @@ from os import getenv
 bind = '127.0.0.1:8001'
 
 workers = multiprocessing.cpu_count() * 2 + 1
-graceful_timeout = 600
-timeout = 300
+# graceful_timeout = 600
+# timeout = 300
 threads = multiprocessing.cpu_count() * 2
-max_requests = 300
+# max_requests = 300
 pidfile = '/var/run/gunicorn.pid'
 max_requests_jitter = 50
 errorlog = '/var/log/gunicorn/gunicorn-error.log'

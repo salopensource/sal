65b4550ecef0e8dc1bbf8dcd12a949d8b29fb356
Graham Gilbert <graham@grahamgilbert.com>
2016-09-15 18:42:56 +0100

    Increase threads

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -2,10 +2,10 @@ import multiprocessing
 from os import getenv
 bind = '127.0.0.1:8001'
 
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() * 3
 # graceful_timeout = 600
-timeout = 60
-threads = multiprocessing.cpu_count() * 2
+# timeout = 60
+threads = multiprocessing.cpu_count() * 3
 # max_requests = 300
 pidfile = '/var/run/gunicorn.pid'
 # max_requests_jitter = 50

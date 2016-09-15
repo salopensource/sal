b5b988c7db988f15343eb5714dc2a79542853a80
Graham Gilbert <graham@grahamgilbert.com>
2016-09-15 11:39:33 +0100

    add back in the timeout

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -4,11 +4,11 @@ bind = '127.0.0.1:8001'
 
 workers = multiprocessing.cpu_count() * 2 + 1
 # graceful_timeout = 600
-# timeout = 300
+timeout = 60
 threads = multiprocessing.cpu_count() * 2
 # max_requests = 300
 pidfile = '/var/run/gunicorn.pid'
-max_requests_jitter = 50
+# max_requests_jitter = 50
 errorlog = '/var/log/gunicorn/gunicorn-error.log'
 loglevel = 'critical'
 # Read the DEBUG setting from env var

73800ed84b8a08f0b56227b4a13415c5a75866e0
Shea Craig <sheagcraig@gmail.com>
2017-01-17 09:16:34 -0500

    Fix query and lint
    - Query was trying to filter for whether one machine was exactly a list
      of machines, which will not work. Updated all three queries to use
      "in".
    - Did the usual style cleanup.

--- server/plugins/munkiinstalls/munkiinstalls.py
+++ server/plugins/munkiinstalls/munkiinstalls.py
@@ -1,12 +1,13 @@
+from datetime import datetime, timedelta, date, time
+from django.db.models import Count
+from django.shortcuts import get_object_or_404
+from django.template import loader, Context
+from server.models import *
 from yapsy.IPlugin import IPlugin
 from yapsy.PluginManager import PluginManager
-from django.template import loader, Context
-from django.db.models import Count
-from server.models import *
-from django.shortcuts import get_object_or_404
-import server.utils as utils
-from datetime import datetime, timedelta, date, time
 import django.utils.timezone
+import server.utils as utils
+
 
 now = django.utils.timezone.now()
 this_day = now -  timedelta(days=0)
@@ -25,7 +26,9 @@ twelve_days_ago = now - timedelta(days=12)
 thirteen_days_ago = now - timedelta(days=13)
 fourteen_days_ago = now - timedelta(days=14)
 
+
 class MunkiInstalls(IPlugin):
+
     def plugin_type(self):
         return 'builtin'
 
@@ -36,9 +39,14 @@ class MunkiInstalls(IPlugin):
         return 'Chart of Munki install activity'
 
     def widget_content(self, page, machines=None, theid=None):
-        # The data is data is pulled from the database and passed to a template.
+        # The data is data is pulled from the database and passed to a
+        # template.
 
-        # There are three possible views we're going to be rendering to - front, bu_dashbaord and group_dashboard. If page is set to bu_dashboard, or group_dashboard, you will be passed a business_unit or machine_group id to use (mainly for linking to the right search).
+        # There are three possible views we're going to be rendering to -
+        # front, bu_dashbaord and group_dashboard. If page is set to
+        # bu_dashboard, or group_dashboard, you will be passed a
+        # business_unit or machine_group id to use (mainly for linking to
+        # the right search).
         if page == 'front':
             t = loader.get_template('munkiinstalls/templates/front.html')
 
@@ -49,20 +57,31 @@ class MunkiInstalls(IPlugin):
             t = loader.get_template('munkiinstalls/templates/front.html')
 
         data = []
-        for time_range in [this_day, one_day_ago, two_days_ago, three_days_ago, four_days_ago,
-        five_days_ago, six_days_ago, seven_days_ago, eight_days_ago, nine_days_ago,
-        ten_days_ago, eleven_days_ago, twelve_days_ago, thirteen_days_ago, fourteen_days_ago]:
+        time_ranges = [
+            this_day, one_day_ago, two_days_ago, three_days_ago, four_days_ago,
+            five_days_ago, six_days_ago, seven_days_ago, eight_days_ago,
+            nine_days_ago, ten_days_ago, eleven_days_ago, twelve_days_ago,
+            thirteen_days_ago, fourteen_days_ago]
+
+        for time_range in time_ranges:
             my_dict = {}
             the_min = datetime.combine(time_range, time.min)
             the_max = datetime.combine(time_range, time.max)
 
-            installs = UpdateHistoryItem.objects.filter(status__iexact='install', recorded__range=(the_min, the_max),
-            update_history__machine=machines, update_history__update_type='third_party').count()
+            installs = UpdateHistoryItem.objects.filter(
+                status__iexact='install', recorded__range=(the_min, the_max),
+                update_history__machine__in=machines,
+                update_history__update_type='third_party').count()
 
-            errors = UpdateHistoryItem.objects.filter(status='error', recorded__range=(the_min, the_max),
-            update_history__machine=machines, update_history__update_type='third_party').count()
+            errors = UpdateHistoryItem.objects.filter(
+                status='error', recorded__range=(the_min, the_max),
+                update_history__machine__in=machines,
+                update_history__update_type='third_party').count()
 
-            pending = UpdateHistoryItem.objects.filter(status='pending', update_history__machine=machines, recorded__range=(the_min, the_max), update_history__update_type='third_party').count()
+            pending = UpdateHistoryItem.objects.filter(
+                status='pending', update_history__machine__in=machines,
+                recorded__range=(the_min, the_max),
+                update_history__update_type='third_party').count()
 
             my_dict['installs'] = installs
             my_dict['pending'] = pending
@@ -79,7 +98,10 @@ class MunkiInstalls(IPlugin):
         return t.render(c)
 
     def filter_machines(self, machines, data):
-        # You will be passed a QuerySet of machines, you then need to perform some filtering based on the 'data' part of the url from the show_widget output. Just return your filtered list of machines and the page title.
+        # You will be passed a QuerySet of machines, you then need to
+        # perform some filtering based on the 'data' part of the url from
+        # the show_widget output. Just return your filtered list of
+        # machines and the page title.
 
         machines = machines.filter(munki_version__exact=data)
 

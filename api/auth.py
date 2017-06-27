a7d370c0b2bcd4c13af99fb21f0c7270bc559580
Graham Gilbert <graham@grahamgilbert.com>
2017-06-27 10:29:19 -0700

    Clean up

--- api/auth.py
+++ api/auth.py
@@ -1,20 +1,3 @@
-# from json_response import *
-# from server.models import *
-# from django.core.exceptions import PermissionDenied
-# import base64
-#
-# def validate_api_key(function):
-#     def wrap(request, *args, **kwargs):
-#         public_key = request.META.get('HTTP_PUBLICKEY', False)
-#         private_key = request.META.get('HTTP_PRIVATEKEY', False)
-#         try:
-#             api_key = ApiKey.objects.get(private_key=private_key, public_key=public_key)
-#         except ApiKey.DoesNotExist:
-#             raise PermissionDenied()
-#         return function(request, *args, **kwargs)
-#     wrap.__doc__=function.__doc__
-#     wrap.__name__=function.__name__
-#     return wrap
 from django.contrib.auth.models import User
 from rest_framework import authentication
 from rest_framework import exceptions

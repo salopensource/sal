da06718bd52e8dbcc8789043230fddf0ba1e47ed
Shea Craig <sheagcraig@gmail.com>
2017-09-07 12:32:17 -0400

    Add django User session handling to custom permission class
    `HasRWPermission.has_object_permission`, at least on current DRF, is
    never called. Moved to `HasRWPermission.has_permission` and expanded to
    handle all three authentication possibilities: (a Sal User,
    AnonymousUser/no auth attempted), an ApiKey.
    
    Also do some minor cleanup.

--- api/auth.py
+++ api/auth.py
@@ -2,40 +2,45 @@ from django.contrib.auth.models import User
 from rest_framework import authentication
 from rest_framework import exceptions
 from rest_framework import permissions
-from server.models import *
+
+from server.models import ApiKey
+
 
 class ApiKeyAuthentication(authentication.BaseAuthentication):
+    """Use publickey/privatekey HTTP headers for authentication"""
+
     def authenticate(self, request):
         public_key = request.META.get('HTTP_PUBLICKEY', False)
         private_key = request.META.get('HTTP_PRIVATEKEY', False)
-        if not public_key or not private_key:
+        if not any((public_key, private_key)):
             return None
 
         try:
-            api_key = ApiKey.objects.get(private_key=private_key, public_key=public_key)
+            api_key = ApiKey.objects.get(
+                private_key=private_key, public_key=public_key)
         except ApiKey.DoesNotExist:
             raise exceptions.AuthenticationFailed('Invalid API Key')
 
         return (api_key, None)
 
+
 class HasRWPermission(permissions.BasePermission):
-    """
-    Only allows RW API keys to write
-    """
+    """Only allow Users with staff status or RW API keys."""
 
-    def has_object_permission(self, request, view, obj):
-        public_key = request.META.get('HTTP_PUBLICKEY', False)
-        private_key = request.META.get('HTTP_PRIVATEKEY', False)
-        if not public_key or not private_key:
+    def has_permission(self, request, view):
+        # Grant Sal Users access based on 'staff' membership; i.e.
+        # 'Global Admin'.
+        if isinstance(request.user, User):
+            return request.user.is_staff
+        # Otherwise, all API token that has passed auth can perform
+        # 'safe' methods.
+        elif isinstance(request.user, ApiKey):
+            if request.method in permissions.SAFE_METHODS:
+                return True
+            # Write permissions are only allowed to keys that have RW.
+            return request.user.read_write
+        else:
+            # If authentication fails, the user will be of type
+            # django.contrib.auth.models.AnonymousUser;
+            # reject anonymous users.
             return False
-
-        try:
-            api_key = ApiKey.objects.get(private_key=private_key, public_key=public_key)
-        except ApiKey.DoesNotExist:
-            raise exceptions.AuthenticationFailed('Invalid API Key')
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        # Write permissions are only allowed to keys that have RW.
-        return api_key.read_write

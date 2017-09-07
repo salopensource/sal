f1ffbdca9611a35f6326f2ae52337a7c3bc36c2c
Shea Craig <sheagcraig@gmail.com>
2017-09-07 12:56:38 -0400

    Use new django-rest-framework explicit field declarations
    This is required now; either fields or excludes (or inherited through
    other serializers).
    
    This gets them all working. More work could be done to determine whether
    we actually want to return all fields!

--- api/serializers.py
+++ api/serializers.py
@@ -1,40 +1,64 @@
 from django.forms import widgets
+
 from rest_framework import serializers
+
 from inventory.models import InventoryItem, Application
 from server.models import *
 
+
 class InventoryApplicationSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Application
+        fields = '__all__'
+
 
 class InventoryItemSerializer(serializers.ModelSerializer):
     application = InventoryApplicationSerializer()
+
     class Meta:
         model = InventoryItem
+        fields = '__all__'
+
 
 class BusinessUnitSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = BusinessUnit
+        fields = '__all__'
+
 
 class MachineGroupSerializer(serializers.ModelSerializer):
     #business_unit = BusinessUnitSerializer()
+
     class Meta:
         model = MachineGroup
+        fields = '__all__'
+
 
 class PluginScriptSubmissionSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = PluginScriptSubmission
+        fields = '__all__'
+
 
 class PluginScriptRowSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = PluginScriptRow
+        fields = '__all__'
+
 
 class FactSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Fact
         exclude = ('machine',)
 
+
 class SerialSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Machine
         fields = ('id','serial',)
@@ -42,33 +66,45 @@ class SerialSerializer(serializers.ModelSerializer):
 
 class FactWithSerialSerializer(serializers.ModelSerializer):
     machine = SerialSerializer()
+
     class Meta:
         model = Fact
+
 
 class ConditionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Condition
         exclude = ('machine',)
 
+
 class ConditionWithSerialSerializer(serializers.ModelSerializer):
     # serial = serializers.CharField()
     machine = SerialSerializer()
+
     class Meta:
         model = Condition
 
+
 class PendingAppleUpdateSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = PendingAppleUpdate
         exclude = ('machine',)
 
+
 class PendingUpdateSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = PendingUpdate
         exclude = ('machine',)
 
+
 class FullMachineSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Machine
+        fields = '__all__'
+
 
 class MachineSerializer(serializers.ModelSerializer):
     # facts = FactSerializer(many=True, required=False)
--- api/urls.py
+++ api/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     url(r'^machines/(?P<serial>.+)/$', views.MachineDetail.as_view()),
     url(r'^machines/$', views.MachineList.as_view()),
     url(r'^machines_full/$', views.MachineListFullDetail.as_view()),
-    # TODO: Add in Application, InventoryItems
     url(r'^inventory/$', views.AllInventory.as_view()),
     url(r'^facts/(?P<serial>.+)/$', views.FactsMachine.as_view()),
     url(r'^facts/$', views.Facts.as_view()),

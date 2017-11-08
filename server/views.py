d49dd48fb1b7cea775dd4e65412e6b10de213ebb
Clayton Burlison <git@clburlison.com>
2017-11-08 11:31:49 -0600

    fix: Update Machine Details Plugin to enable vi UI
    Credit to grahamgilbert

--- server/views.py
+++ server/views.py
@@ -1666,24 +1666,23 @@ def machine_detail_plugin_enable(request, plugin_name):
     # only do this if there isn't a plugin already with the name
     try:
         plugin = MachineDetailPlugin.objects.get(name=plugin_name)
-    except Plugin.DoesNotExist:
-        enabled_plugins = Plugin.objects.all()
+    except MachineDetailPlugin.DoesNotExist:
+        enabled_plugins = MachineDetailPlugin.objects.all()
         # Build the manager
         manager = PluginManager()
         # Tell it the default place(s) where to find plugins
         manager.setPluginPlaces([settings.PLUGIN_DIR, os.path.join(settings.PROJECT_DIR, 'server/plugins')])
         # Load all plugins
         manager.collectPlugins()
-        enabled_plugins = apps.get_model("server", "MachineDetailPlugin")
-        for item in enabled_plugins.objects.all():
-            default_families = ['Darwin', 'Windows', 'Linux']
-            for plugin in manager.getAllPlugins():
-                if plugin.name == item.name:
 
-                    try:
-                        supported_os_families = plugin.plugin_object.supported_os_families()
-                    except:
-                        supported_os_families = default_families
+        default_families = ['Darwin', 'Windows', 'Linux']
+        for plugin in manager.getAllPlugins():
+            if plugin.name == plugin_name:
+
+                try:
+                    supported_os_families = plugin.plugin_object.supported_os_families()
+                except:
+                    supported_os_families = default_families
         plugin = MachineDetailPlugin(name=plugin_name, order=utils.UniquePluginOrder(plugin_type='machine_detail'), os_families=utils.flatten_and_sort_list(supported_os_families))
         plugin.save()
     return redirect('settings_machine_detail_plugins')

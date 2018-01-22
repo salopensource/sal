76dcb1b54b24a8f4f39e4b10550d3725744da205
Shea Craig <sheagcraig@gmail.com>
2018-01-22 13:02:21 -0500

    Use args to restrict API docs site to v2.

--- api/v2/urls.py
+++ api/v2/urls.py
@@ -21,7 +21,12 @@ router.register(r'saved_searches', views.SavedSearchViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^docs/', include_docs_urls(title='Sal REST API')),
     url(r'^api-auth/', include(
         'rest_framework.urls', namespace='rest_framework')),
 ]
+
+# Include the docs separatly, so we can use the v2 URLs above to filter
+# out the older API schema from the docs.
+urlpatterns.append(
+    url(r'^docs/', include_docs_urls(
+        title='Sal REST API', schema_url='/api/v2/', patterns=urlpatterns)))

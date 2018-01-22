38ef8338ac35474038216484eaa37763b85bbc21
Shea Craig <sheagcraig@gmail.com>
2018-01-22 09:41:25 -0500

    Remove unused imports.

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import include, url
 
 from rest_framework.documentation import include_docs_urls
-from rest_framework.routers import SimpleRouter, DefaultRouter
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
 from api import views
 

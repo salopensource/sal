5e39fafab530f6e8f05306c2ee3fca2ab7e0c679
Shea Craig <sheagcraig@gmail.com>
2018-01-23 09:59:20 -0500

    Start abstracting common test tasks out.

--- api/v2/tests.py
+++ api/v2/tests.py
@@ -4,14 +4,29 @@ from rest_framework.test import APITestCase
 from server.models import *
 
 
-class MachinesTest(APITestCase):
+class SalAPITestCase(APITestCase):
+    setup_data = []
 
     def setUp(self):
+        # Set up an APIKey for authenticated tests to use.
         api_key = ApiKey.objects.create()
-        self.headers = {'HTTP_PRIVATEKEY': api_key.private_key, 'HTTP_PUBLICKEY': api_key.public_key}
+        self.headers = {
+            'HTTP_PRIVATEKEY': api_key.private_key,
+            'HTTP_PUBLICKEY': api_key.public_key}
+
+        for data_method_name in self.setup_data:
+            data_method = getattr(self, data_method_name)
+            data_method()
+
+    def create_machine_data(self):
         bu = BusinessUnit.objects.create(name='test')
         mg = MachineGroup.objects.create(name='test', business_unit=bu)
         Machine.objects.create(serial='C0DEADBEEF', machine_group=mg)
+
+
+class MachinesTest(SalAPITestCase):
+    setup_data = ['create_machine_data']
+
 
     def test_access(self):
         response = self.client.get(reverse('machine-list'))
@@ -24,16 +39,23 @@ class MachinesTest(APITestCase):
         self.assertEqual(response['content-type'], 'application/json')
 
     def test_get_by_serial(self):
-        response = self.client.get(reverse('machine-detail', args=('C0DEADBEEF',)), {}, **self.headers)
+        response = self.client.get(
+            reverse('machine-detail', args=('C0DEADBEEF',)), {},
+            **self.headers)
         self.assertEqual(response.status_code, 200)
 
     def test_get_by_id_returns_404(self):
-        response = self.client.get(reverse('machine-detail', args=(1,)), {}, **self.headers)
+        response = self.client.get(
+            reverse('machine-detail', args=(1,)), {}, **self.headers)
         self.assertEqual(response.status_code, 404)
 
     def test_get_with_full(self):
-        response = self.client.get(reverse('machine-detail', args=('C0DEADBEEF',)), {}, **self.headers)
-        full_response = self.client.get(reverse('machine-detail', args=('C0DEADBEEF',)), {'full': None}, **self.headers)
+        response = self.client.get(
+            reverse('machine-detail', args=('C0DEADBEEF',)), {},
+            **self.headers)
+        full_response = self.client.get(
+            reverse('machine-detail', args=('C0DEADBEEF',)), {'full': None},
+            **self.headers)
         short = response.json()
         full = full_response.json()
         self.assertNotEqual(len(short), len(full))

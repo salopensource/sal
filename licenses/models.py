f86078d096f46a62e71c97a8cf17ca5c9836266d
Shea Craig <sheagcraig@gmail.com>
2018-01-29 13:39:44 -0500

    Update urllib imports to py3.

--- licenses/models.py
+++ licenses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from server.models import BusinessUnit
 from inventory.models import InventoryItem
 
-from urllib import quote_plus
+from urllib.parse import quote_plus
 
 class License(models.Model):
     item_name = models.CharField(max_length=64, unique=True, primary_key=True)

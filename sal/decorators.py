88dbeccff5121fe160613fa589fad1bb7348842c
Shea Craig <sheagcraig@gmail.com>
2018-01-30 11:43:39 -0500

    Block users without access to a BU from getting 404 for missing objects.

--- sal/decorators.py
+++ sal/decorators.py
@@ -3,6 +3,7 @@
 
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.http.response import Http404
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.generic import View
@@ -43,9 +44,15 @@ def class_access_required(cls):
             # The request object is the first arg to a view
             request = args[0]
             user = request.user
-            business_unit = cls.get_business_unit(**kwargs)
+            try:
+                business_unit = cls.get_business_unit(**kwargs)
+            except Http404:
+                business_unit = Http404
 
             if is_global_admin(user) or has_access(user, business_unit):
+                # Hide the 404 response from users without perms.
+                if business_unit is Http404:
+                    raise Http404
                 return f(*args, **kwargs)
             else:
                 raise PermissionDenied()
@@ -59,42 +66,6 @@ def class_access_required(cls):
 def is_global_admin(user):
     return user.userprofile.level == "GA"
 
-# def view_or_basicauth(view, request, *args, **kwargs):
-#     # Check for valid basic auth header
-#     if hasattr(settings, 'BASIC_AUTH'):
-#         use_auth = settings.BASIC_AUTH
-#     else:
-#         use_auth = True
-#
-#     if use_auth == False:
-#         return view(request, *args, **kwargs)
-#
-#     if 'HTTP_AUTHORIZATION' in request.META:
-#         auth = request.META['HTTP_AUTHORIZATION'].split()
-#         if len(auth) == 2:
-#             if auth[0].lower() == "basic":
-#                 uname, key = base64.b64decode(auth[1]).split(':')
-#                 try:
-#                     machine_group = MachineGroup.objects.get(key=key)
-#                 except MachineGroup.DoesNotExist:
-#                     machine_group = None
-#
-#                 if machine_group is not None and uname == 'sal':
-#                     return view(request, *args, **kwargs)
-#
-#     # Either they did not provide an authorization header or
-#     # something in the authorization attempt failed. Send a 401
-#     # back to them to ask them to authenticate.
-#     response = HttpResponse()
-#     response.status_code = 401
-#     response['WWW-Authenticate'] = 'Basic realm="%s"' % settings.BASIC_AUTH_REALM
-#     return response
-#
-#
-# def key_auth_required(view_func):
-#     def wrapper(request, *args, **kwargs):
-#         return view_or_basicauth(view_func, request, *args, **kwargs)
-#     return wrapper(view_func)
 
 def key_auth_required(function):
     def wrap(request, *args, **kwargs):
@@ -133,5 +104,5 @@ def key_auth_required(function):
 
 
 def has_access(user, business_unit):
-    return (business_unit and
-            business_unit in user.businessunit_set.all())
+    return (
+        business_unit and business_unit in user.businessunit_set.all())

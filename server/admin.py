a52b32270b733833a80523bb6b81c9c12545831d
Shea Craig <sheagcraig@gmail.com>
2018-02-09 15:47:22 -0500

    Add some more displayed fields to lists and some filters.

--- server/admin.py
+++ server/admin.py
@@ -25,7 +25,6 @@ class BusinessUnitForm(ModelForm):
         widget=admin.widgets.FilteredSelectMultiple(verbose_name="Users", is_stacked=False))
 
 
-
 def number_of_users(obj):
     return obj.users.count()
 
@@ -46,6 +45,48 @@ class BusinessUnitAdmin(admin.ModelAdmin):
     readonly_fields = (number_of_users, number_of_machine_groups, number_of_machines)
 
 
+class BusinessUnitFilter(admin.SimpleListFilter):
+    title = 'Business Unit'
+    parameter_name = 'business_unit'
+
+    def lookups(self, request, model_admin):
+        return ((bu.name, bu.name) for bu in BusinessUnit.objects.all())
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(machine__machine_group__business_unit__name=self.value())
+        else:
+            return queryset
+
+
+class MachineGroupFilter(admin.SimpleListFilter):
+    title = 'Machine Group'
+    parameter_name = 'machine_group'
+
+    def lookups(self, request, model_admin):
+        return ((mg.name, mg.name) for mg in MachineGroup.objects.all())
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(machine__machine_group__name=self.value())
+        else:
+            return queryset
+
+
+class ConditionAdmin(admin.ModelAdmin):
+    list_filter = (BusinessUnitFilter, MachineGroupFilter, 'condition_name')
+
+
+class FactAdmin(admin.ModelAdmin):
+    list_display = ('fact_name', 'fact_data')
+    list_filter = (BusinessUnitFilter, MachineGroupFilter, 'fact_name')
+
+
+class HistoricalFactAdmin(admin.ModelAdmin):
+    list_display = ('fact_name', 'fact_data', 'fact_recorded')
+    list_filter = (BusinessUnitFilter, MachineGroupFilter, 'fact_name')
+
+
 class MachineAdmin(admin.ModelAdmin):
     list_display = ('hostname', 'serial')
 
@@ -56,9 +97,9 @@ class MachineGroupAdmin(admin.ModelAdmin):
 
 admin.site.register(ApiKey, ApiKeyAdmin)
 admin.site.register(BusinessUnit, BusinessUnitAdmin)
-admin.site.register(Condition)
-admin.site.register(Fact)
-admin.site.register(HistoricalFact)
+admin.site.register(Condition, ConditionAdmin)
+admin.site.register(Fact, FactAdmin)
+admin.site.register(HistoricalFact, HistoricalFactAdmin)
 admin.site.register(InstalledUpdate)
 admin.site.register(Machine, MachineAdmin)
 admin.site.register(MachineDetailPlugin)

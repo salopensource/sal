19dc8b7e1535c4cc431b765f95db117175fc7d24
Shea Craig <sheagcraig@gmail.com>
2018-02-09 08:52:07 -0500

    Sort registrations. Separate classes of imports. Add API key display.

--- server/admin.py
+++ server/admin.py
@@ -1,33 +1,38 @@
 from django.contrib import admin
+
 from server.models import *
 
 
-class MachineGroupAdmin(admin.ModelAdmin):
-    readonly_fields = ('key',)
+class ApiKeyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'public_key', 'private_key')
 
 
 class MachineAdmin(admin.ModelAdmin):
     list_display = ('hostname', 'serial')
 
 
-admin.site.register(UserProfile)
+class MachineGroupAdmin(admin.ModelAdmin):
+    readonly_fields = ('key',)
+
+
+admin.site.register(ApiKey, ApiKeyAdmin)
 admin.site.register(BusinessUnit)
-admin.site.register(MachineGroup, MachineGroupAdmin)
-admin.site.register(Machine, MachineAdmin)
-admin.site.register(Fact)
-admin.site.register(PluginScriptSubmission)
-admin.site.register(PluginScriptRow)
-admin.site.register(HistoricalFact)
 admin.site.register(Condition)
-admin.site.register(PendingUpdate)
+admin.site.register(Fact)
+admin.site.register(HistoricalFact)
 admin.site.register(InstalledUpdate)
-admin.site.register(PendingAppleUpdate)
-admin.site.register(ApiKey)
-admin.site.register(Plugin)
-admin.site.register(Report)
-# admin.site.register(OSQueryResult)
+admin.site.register(Machine, MachineAdmin)
+admin.site.register(MachineDetailPlugin)
+admin.site.register(MachineGroup, MachineGroupAdmin)
 # admin.site.register(OSQueryColumn)
+# admin.site.register(OSQueryResult)
+admin.site.register(PendingAppleUpdate)
+admin.site.register(PendingUpdate)
+admin.site.register(Plugin)
+admin.site.register(PluginScriptRow)
+admin.site.register(PluginScriptSubmission)
+admin.site.register(Report)
 admin.site.register(SalSetting)
 admin.site.register(UpdateHistory)
 admin.site.register(UpdateHistoryItem)
-admin.site.register(MachineDetailPlugin)
+admin.site.register(UserProfile)

5e2f591300087c193c46fa53f0f8fc62b8b88a69
Shea Craig <sheagcraig@gmail.com>
2018-02-09 11:10:23 -0500

    Add multi-select users widget, and counts of users, machines, m groups.

--- server/admin.py
+++ server/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.forms import ModelForm, ModelMultipleChoiceField
 
 from server.models import *
 
@@ -11,8 +12,40 @@ class MachineGroupInline(admin.TabularInline):
     model = MachineGroup
 
 
+class BusinessUnitForm(ModelForm):
+
+    class Meta:
+        model = BusinessUnit
+        fields = ['name', 'users']
+
+    users = ModelMultipleChoiceField(
+        queryset=User.objects.exclude(userprofile__level='GA'),
+        label=('Members'),
+        required=False,
+        widget=admin.widgets.FilteredSelectMultiple(verbose_name="Users", is_stacked=False))
+
+
+
+def number_of_users(obj):
+    return obj.users.count()
+
+
+def number_of_machine_groups(obj):
+    return obj.machinegroup_set.count()
+
+
+def number_of_machines(obj):
+    return Machine.objects.filter(machine_group__in=obj.machinegroup_set.all()).count()
+
+
 class BusinessUnitAdmin(admin.ModelAdmin):
     inlines = [MachineGroupInline,]
+    form = BusinessUnitForm
+    list_display = ('name', number_of_users, number_of_machine_groups, number_of_machines)
+    fields = (('name', number_of_users, number_of_machine_groups, number_of_machines), 'users')
+    readonly_fields = (number_of_users, number_of_machine_groups, number_of_machines)
+
+
 class MachineAdmin(admin.ModelAdmin):
     list_display = ('hostname', 'serial')
 
@@ -22,7 +55,7 @@ class MachineGroupAdmin(admin.ModelAdmin):
 
 
 admin.site.register(ApiKey, ApiKeyAdmin)
-admin.site.register(BusinessUnit)
+admin.site.register(BusinessUnit, BusinessUnitAdmin)
 admin.site.register(Condition)
 admin.site.register(Fact)
 admin.site.register(HistoricalFact)

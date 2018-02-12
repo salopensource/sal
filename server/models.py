b6042bf52c0b77ac9ce7737ed3dcbe8a92af0630
Shea Craig <sheagcraig@gmail.com>
2018-02-12 11:54:50 -0500

    Remove types that a `MachineDetailPlugin` should not have and migrate.

--- server/models.py
+++ server/models.py
@@ -464,17 +464,14 @@ class Plugin(models.Model):
 
 class MachineDetailPlugin(models.Model):
     PLUGIN_TYPES = (
-        ('facter', 'Facter'),
-        ('munkicondition', 'Munki Condition'),
-        ('builtin', 'Built In'),
-        ('custom', 'Custom Script'),
+        ('machine_detail', 'Machine Detail'),
     )
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
     order = models.IntegerField()
     os_families = models.CharField(db_index=True, max_length=256,
                                    verbose_name="OS Family", default="Darwin")
-    type = models.CharField(max_length=255, choices=PLUGIN_TYPES, default='builtin')
+    type = models.CharField(max_length=255, choices=PLUGIN_TYPES, default='machine_detail')
 
     def __unicode__(self):
         return self.name

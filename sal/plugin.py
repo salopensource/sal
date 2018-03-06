26d1d761edfb6053579c89345be29ebc932399b4
Shea Craig <sheagcraig@gmail.com>
2018-03-05 20:00:48 -0500

    Move some defaults around in plugin classes, add docstrings.
    This moves the machine filtration code to a mixin.

--- sal/plugin.py
+++ sal/plugin.py
@@ -29,17 +29,16 @@ class BasePlugin(IPlugin):
 
     Public Methods:
         plugin_type
-        supported_os_families
         widget_width
         get_description
+        get_template
 
         widget_content: Returns rendered content of the plugin.
         get_context: All subclasses need to reimplement this.
     """
 
     _description = ''
-    _plugin_type = 'builtin'
-    _supported_os_families = [OSFamilies.darwin]
+    _plugin_type = 'base'
     _template = ''
     _widget_width = 4
 
@@ -53,8 +52,6 @@ class BasePlugin(IPlugin):
             description (str): Plugin description. Defaults to ''.
             plugin_type (PluginType): Type of plugin. Defaults to
                 'builtin'.
-            supported_os_families (list of OSFamilies): OSes this
-                plugin can handle. Defaults to [OSFamiles.darwin].
             template (str): Subpath to plugin's template. If not
                 specified, will try to use the plugin's class
                 name, lowercased, formatted into
@@ -62,30 +59,44 @@ class BasePlugin(IPlugin):
             widget_width (int): Width of the plugin's widget. Defaults
                 to 4.
         """
-        description = ''
-        plugin_type = PluginTypes.builtin
-        supported_os_families = [OSFamilies.darwin]
-        template = ''
-        widget_width = 4
+        pass
 
     def __init__(self):
         super(BasePlugin, self).__init__()
-        self.template = loader.get_template(self._get_template())
 
     def _get_from_meta_or_default(self, name):
         return getattr(self.Meta, name, None) or getattr(self, "_" + name, None)
 
-    def _get_template(self):
+    def get_template(self, *args, **kwargs):
+        """Get the plugin's django template.
+
+        This method by default looks up the self._template attribute.
+        If that's not available, it tries to construct a template path
+        from the name of the plugin's class, lowercased. (i.e. BasePlugin
+        would have a computed template path of
+        'baseplugin/templates/baseplugin.html').
+
+        To allow this method to be overridden by clients, it accepts
+        *args and **kwargs. See the `widget_content` method to see the
+        primary use of this method; it's called with the machines
+        queryset, the `group_type`, and `group_id`, so an overriden
+        `get_template` could select from different templates based
+        on the contextual data.
+
+        Args:
+            args: Unused in this implementation.
+            kwargs: Unused in this implementation.
+
+        Returns:
+            Django template for this plugin.
+        """
         template = self._get_from_meta_or_default('template')
         if not template:
             template = "{0}/templates/{0}.html".format(self.__class__.__name__.lower())
-        return template
+        return loader.get_template(template)
 
     def plugin_type(self):
         return self._get_from_meta_or_default('plugin_type')
-
-    def supported_os_families(self):
-        return self._get_from_meta_or_default('supported_os_families')
 
     def widget_width(self):
         return self._get_from_meta_or_default('widget_width')
@@ -93,14 +104,11 @@ class BasePlugin(IPlugin):
     def get_description(self):
         return self._get_from_meta_or_default('description')
 
-    def widget_content(self, machines, group_type=None, group_id=None):
-        if hasattr(self, 'get_context'):
-            context = self.get_context(machines, group_type=group_type, group_id=group_id)
-        else:
-            context = {}
-        return self.template.render(context)
+    def widget_content(self, machines, group_type='all', group_id=None):
+        context = self.get_context(machines, group_type=group_type, group_id=group_id)
+        return self.get_template(machines, group_type, group_id).render(context)
 
-    def get_context(self, machines, group_type=None, group_id=None):
+    def get_context(self, machines, group_type, group_id):
         """Process input into a context suitable for rendering.
 
         This method must be overridden by subclasses; the base class
@@ -111,9 +119,8 @@ class BasePlugin(IPlugin):
                 consider while generating plugin's output.
             group_type (str): One of 'all', 'business_unit',
                 'machine_group', or 'machine', determining the limits of
-                the machines query. Optional, defaults to None.
+                the machines query.
             group_id (int): Primary key value of the associated group.
-                Optional, defaults to None.
 
         Returns:
             Dict suitable for plugin's template to render.
@@ -121,8 +128,8 @@ class BasePlugin(IPlugin):
         return {}
 
 
-class MachinesPlugin(BasePlugin):
-    """Base class for all dashboard plugins.
+class FilterMixin(object):
+    """Adds filter_machines method to Plugins
 
     Public_Methods
         filter_machines: Filter passed machines using filter method.
@@ -136,8 +143,7 @@ class MachinesPlugin(BasePlugin):
     def filter(self, machines, data):
         """Filter machines further for redirect to a machine list view
 
-        This method is used when a link originating from the output of
-        the plugin needs to redirect to a machine list view. This
+        This method is used when a link originating from the output of the plugin needs to redirect to a machine list view. This
         method is then used by the machine list view to filter the
         machines prior to list display.
 
@@ -155,14 +161,28 @@ class MachinesPlugin(BasePlugin):
         return machines, data
 
 
+class MachinesPlugin(FilterMixin, BasePlugin):
+
+    _plugin_type = 'builtin'
+
+
 class DetailPlugin(BasePlugin):
+    """Machine Detail plugin class.
 
+    Public Methods:
+        supported_os_families
+    """
     _plugin_type = 'machine_detail'
+    _supported_os_families = [OSFamilies.darwin]
+
+    def supported_os_families(self):
+        return self._get_from_meta_or_default('supported_os_families')
 
 
-class ReportPlugin(BasePlugin):
+class ReportPlugin(FilterMixin, BasePlugin):
 
     _plugin_type = 'report'
+    _widget_width = 12
 
 
 # Load all plugins at import time. Then all plugin users can call

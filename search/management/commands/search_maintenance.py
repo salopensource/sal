17a3126a1b06a582228526617d1a7b867dcb1771
Shea Craig <sheagcraig@gmail.com>
2018-03-29 09:53:13 -0400

    Fix lint error.

--- search/management/commands/search_maintenance.py
+++ search/management/commands/search_maintenance.py
@@ -151,7 +151,6 @@ class Command(BaseCommand):
         if server.utils.is_postgres() and items_to_be_inserted != []:
             SearchCache.objects.bulk_create(items_to_be_inserted)
 
-
         # Clean up orhpaned Application objects.
         Application.objects.filter(inventoryitem=None).delete()
 

581a85693d4d917d648fdb19689732e871a79724
Graham Gilbert <graham@grahamgilbert.com>
2018-04-13 12:17:30 -0700

    Testing the tests

--- profiles/tests/test_admin.py
+++ profiles/tests/test_admin.py
@@ -3,4 +3,4 @@ from sal.test_utils import AdminTestCase
 
 class ProfileAdminTest(AdminTestCase):
     """Test the admin site is configured to have all expected views."""
-    admin_endpoints = {'profile', 'payload'}
+    admin_endpoints = {'profile'}

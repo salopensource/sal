3acc25f8a314adb4af36d07097d61cbef95a1565
Graham Gilbert <graham@grahamgilbert.com>
2018-04-18 06:57:13 -0700

    Linted

--- server/plugins/encryption/encryption.py
+++ server/plugins/encryption/encryption.py
@@ -9,7 +9,7 @@ import sal.plugin
 plugin_q = Q(pluginscriptsubmission__plugin='Encryption')
 # The name got changed from Filevault to FileVault. Support both.
 name_q = Q(pluginscriptsubmission__pluginscriptrow__pluginscript_name__exact='Filevault') | \
-        Q(pluginscriptsubmission__pluginscriptrow__pluginscript_name__exact='FileVault')
+    Q(pluginscriptsubmission__pluginscriptrow__pluginscript_name__exact='FileVault')
 enabled_q = Q(pluginscriptsubmission__pluginscriptrow__pluginscript_data='Enabled')
 disabled_q = Q(pluginscriptsubmission__pluginscriptrow__pluginscript_data='Disabled')
 portable_q = Q(machine_model__contains='Book')

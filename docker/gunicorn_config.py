caa100912f2751188b21226faea1c7e5187fc04f
Graham Gilbert <graham@grahamgilbert.com>
2018-06-22 16:13:36 -0700

    Up the timeout, workers get killed too easily

--- docker/gunicorn_config.py
+++ docker/gunicorn_config.py
@@ -3,8 +3,8 @@ from os import getenv
 bind = '127.0.0.1:8001'
 
 workers = multiprocessing.cpu_count() * 3
-graceful_timeout = 15
-timeout = 30
+graceful_timeout = 60
+timeout = 90
 threads = multiprocessing.cpu_count() * 3
 
 pidfile = '/var/run/gunicorn.pid'

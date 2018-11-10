5d0a661c5d667467d990a2f56445cf52fb88501a
Shea Craig <sheagcraig@gmail.com>
2018-11-10 10:49:19 -0500

    Update version getter with Pathlib, fix plist loading, and test.

--- server/tests/test.py
+++ server/tests/test.py
@@ -1,6 +1,8 @@
 """General functional tests for the server app."""
 
 
+import unittest.mock
+
 from django.test import TestCase
 
 import sal.plugin
@@ -22,3 +24,17 @@ class PluginUtilsTest(TestCase):
         self.assertEqual(Plugin.objects.count(), 0)
         utils.load_default_plugins()
         self.assertNotEqual(Plugin.objects.count(), 0)
+
+
+class ServerUtilsTest(TestCase):
+    """Test the server app utilities"""
+
+    @unittest.mock.patch('pathlib.Path.open', new_callable=unittest.mock.mock_open())
+    @unittest.mock.patch('plistlib.load')
+    def test_get_server_version(self, mock_load, mock_open):
+        """Test that the server version getter works"""
+        version = '4.0.0'
+        mock_load.return_value = {'version': version}
+        version_result = utils.get_server_version()
+
+        self.assertEqual(version_result, version)

169d4b1c71ad651eb00c6e284e38578d5a532cfd
Shea Craig <sheagcraig@gmail.com>
2018-11-11 15:44:05 -0500

    Make plist content for Catalog a BinaryField, and cleanup sub views.

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,7 @@ from server.models import *
 
 class Catalog(models.Model):
     machine_group = models.ForeignKey(MachineGroup)
-    content = models.TextField()
+    content = models.BinaryField()
     name = models.CharField(max_length=253)
     sha256hash = models.CharField(max_length=64)
 

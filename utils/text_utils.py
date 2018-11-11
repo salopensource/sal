8e1d3a3b1251a518352e377371077b4aa57df7da
Shea Craig <sheagcraig@gmail.com>
2018-11-11 15:43:25 -0500

    Rename submission decoder and update clients.

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -1,14 +1,20 @@
 import base64
 import bz2
+import plistlib
 import re
-import typing
+from typing import Any, Union, Dict
+from xml.parsers.expat import ExpatError
+
+
+Plist = Dict[str, Any]
+Text = Union[str, bytes]
 
 
 def class_to_title(text):
     return re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ', text)
 
 
-def safe_text(text: typing.Any) -> str:
+def safe_text(text: Any) -> str:
     """Process text of any type to ensure it can be saved in the DB."""
     # Ensure bytes get decoded, no matter what, to unicode.
     if isinstance(text, bytes):
@@ -40,33 +46,63 @@ def stringify(data):
     return str(data)
 
 
-def decode_to_string(data, compression=''):
-    """Decodes a string optionally bz2 compressed and/or base64 encoded.
+def decode_submission_data(data: Text, compression: str = '') -> bytes:
+    """Returns bytes from compressed or base64 encoded text.
 
-    Will decode base64 first if specified. Then decompress bz2 if
-    specified.
+    Sal submissions include plist data that has been base64 encoded
+    and bz2 coompressed. Historically, this data may also have been
+    passed as just text, or only base64 encoded.
+
+    This function first decodes from base64 to bytes. Then it will
+    perforom bz2 decompression if requested. As this data is normally
+    then parsed through plistlib, the data is returned as bytes under
+    all circumstances.
 
     Args:
-        data (str): Data to decode. May just be a regular string!
+        data (str, bytes): Data to decode. May just be a regular string!
         compression (str): Type of encoding and compression. Defaults
             to empty str. Uses substrings to determine what to do:
             'base64' results in first base64 decoding.
             'bz2' results in bz2.decompression.
 
     Returns:
-        The decoded, decompressed string, or '' if there were
+        The decoded, decompressed bytes, or b'' if there were
         exceptions.
     """
     if 'base64' in compression:
         try:
             data = base64.b64decode(data)
         except TypeError:
-            data = ''
+            data = b''
 
     if 'bz2' in compression:
         try:
             data = bz2.decompress(data)
         except IOError:
-            data = ''
+            data = b''
+
+    # Make sure we're returning bytes, even if the compression
+    # arg is empty.
+    if isinstance(data, str):
+        data = data.encode()
 
     return data
+
+
+def submission_plist_loads(data: Text, compression: str = '') -> Plist:
+    decoded = decode_submission_data(data, compression)
+    try:
+        plist = plistlib.loads(decoded)
+    except (plistlib.InvalidFileException, ExpatError):
+        plist = {}
+    return plist
+
+
+def is_valid_plist(data: Text) -> bool:
+    if isinstance(data, str):
+        data = data.encode()
+    try:
+        _ = plistlib.loads(data)
+        return True
+    except (plistlib.InvalidFileException, ExpatError):
+        return False

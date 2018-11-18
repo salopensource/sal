8269f4e2043bf47ef76a19e9c70d86154095da16
Graham Gilbert <graham@grahangilbert.com>
2018-11-18 22:08:12 +0000

    Quick post script

--- remote_build.py
+++ remote_build.py
@@ -10,3 +10,16 @@ parser.add_argument('build_tag', type=str, help='The tag to build.')
 
 args = parser.parse_args()
 print args.build_tag
+
+project_username = os.getenv('CIRCLE_PROJECT_USERNAME')
+api_user_token = os.getenv('CIRCLE_API_USER_TOKEN')
+project_reponame = os.getenv('CIRCLE_PROJECT_REPONAME')
+
+post_data = {}
+post_data['build_parameters'] = {'TAG':args.build_tag}
+
+url = "https://circleci.com/api/v1.1/project/github/{}/{}/tree/{}".format(project_username, project_reponame, args.build_tag)
+
+
+
+the_request = requests.post(url, json=post_data, auth=(api_user_token, ''))

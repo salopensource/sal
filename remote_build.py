99751f768c4ae8fede76e409b234eef4030eeb45
Graham Gilbert <graham@grahangilbert.com>
2018-11-18 22:13:10 +0000

    Linter

--- remote_build.py
+++ remote_build.py
@@ -16,10 +16,12 @@ api_user_token = os.getenv('CIRCLE_API_USER_TOKEN')
 project_reponame = os.getenv('CIRCLE_PROJECT_REPONAME')
 
 post_data = {}
-post_data['build_parameters'] = {'TAG':args.build_tag}
+post_data['build_parameters'] = {'TAG': args.build_tag}
 
-url = "https://circleci.com/api/v1.1/project/github/{}/{}/tree/{}".format(project_username, project_reponame, args.build_tag)
-
-
+url = "https://circleci.com/api/v1.1/project/github/{}/{}/tree/{}".format(
+    project_username,
+    project_reponame,
+    args.build_tag
+)
 
 the_request = requests.post(url, json=post_data, auth=(api_user_token, ''))

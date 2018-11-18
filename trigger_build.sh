3bef2fe755f8083a62b98b1bfa562b5d93201361
Graham Gilbert <graham@grahangilbert.com>
2018-11-18 01:20:17 +0000

    tag it right

--- trigger_build.sh
+++ trigger_build.sh
@@ -7,9 +7,15 @@ else
     BRANCH="master"
 fi
 
+if [ "$BRANCH" != "master" ]; then
+    TAG="$BRANCH"
+else
+    TAG="latest"
+fi
+
 URL="https://circleci.com/api/v1.1/project/github/salopensource/sal/tree/${BRANCH}"
 
-jq -n '{TAG: "latest"}' | curl -X POST -d @- \
+jq -n '{TAG: "${TAG}"' | curl -X POST -d @- \
   --user ${CIRCLE_API_USER_TOKEN}: \
   --url $URL \
   -H 'Content-Type: application/json' \

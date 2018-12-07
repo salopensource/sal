c0c1a64c8b79438950fdb21e1bb1c806e674f51e
Shea Craig <sheagcraig@gmail.com>
2018-12-07 10:31:00 -0500

    Fix flake8 errors.

--- server/management/commands/friendly_model_name.py
+++ server/management/commands/friendly_model_name.py
@@ -12,10 +12,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Get all the machines without a friendly model name and have a model
-        no_friendly = Machine.objects.filter(
-            Q(machine_model_friendly__isnull=True) |
-            Q(machine_model_friendly='')
-        ).exclude(machine_model__isnull=True).exclude(machine_model='').filter(os_family='Darwin')
+        no_friendly = (
+            Machine.objects.filter(
+                Q(machine_model_friendly__isnull=True) | Q(machine_model_friendly=''))
+            .exclude(machine_model__isnull=True)
+            .exclude(machine_model='')
+            .filter(os_family='Darwin')
         for machine in no_friendly[:100]:
             print(f'Processing {machine}')
             machine.machine_model_friendly = utils.friendly_machine_model(machine)

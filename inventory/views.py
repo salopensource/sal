4857f4ede0927d3e4a4eb525dd5a8fb281cb9fa2
Shea Craig <sheagcraig@gmail.com>
2018-12-08 10:31:25 -0500

    Use new `reverse` location.

--- inventory/views.py
+++ inventory/views.py
@@ -7,10 +7,10 @@ from distutils.version import LooseVersion
 from urllib.parse import quote
 
 # Django
-from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST

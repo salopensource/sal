22725d726b345bd539ce75cdb4a3648d168404a8
Shea Craig <sheagcraig@gmail.com>
2018-12-11 08:21:22 -0500

    Clean up urls.

--- sal/urls.py
+++ sal/urls.py
@@ -1,18 +1,17 @@
-from django.conf.urls import include, url
+import django.contrib.auth.views as auth_views
 from django.conf import settings
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-admin.autodiscover()
-import django.contrib.auth.views as auth_views
 from django.contrib.staticfiles import views
 
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^login/*$', auth_views.LoginView, name='login'),
     url(r'^logout/$', auth_views.logout_then_login, name='logout_then_login'),
     url(r'^changepassword/$', auth_views.PasswordChangeView, name='password_change'),
-    url(
-        r'^changepassword/done/$', auth_views.PasswordChangeDoneView, name='password_change_done'),
+    url(r'^changepassword/done/$', auth_views.PasswordChangeDoneView, name='password_change_done'),
     url(r'^', include('server.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', admin.site.urls),
@@ -27,8 +26,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += [
-        url(r'^static/(?P<path>.*)$', views.serve),
-    ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += [url(r'^static/(?P<path>.*)$', views.serve),]

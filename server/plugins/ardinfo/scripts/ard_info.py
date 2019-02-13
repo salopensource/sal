04494650050b55c8292e8a0727cdce2efe733872
Shea Craig <sheagcraig@gmail.com>
2019-02-13 15:51:56 -0500

    Remove `add_plugin_results` func from plugin scripts.
    This has safely been in sal-scripts for a long time now.

--- server/plugins/ardinfo/scripts/ard_info.py
+++ server/plugins/ardinfo/scripts/ard_info.py
@@ -6,6 +6,8 @@ import sys
 
 sys.path.append("/usr/local/munki/munkilib")
 import FoundationPlist
+sys.path.append("/usr/local/sal")
+import utils
 
 
 def main():
@@ -22,29 +24,7 @@ def main():
         sal_result_key.format(i): ard_prefs.get(prefs_key_prefix.format(i), "")
         for i in range(1, 5)}
 
-    add_plugin_results('ARD_Info', data)
-
-
-def add_plugin_results(plugin, data, historical=False):
-    """Add data to the shared plugin results plist.
-
-    This function creates the shared results plist file if it does not
-    already exist; otherwise, it adds the entry by appending.
-
-    Args:
-        plugin (str): Name of the plugin returning data.
-        data (dict): Dictionary of results.
-        historical (bool): Whether to keep only one record (False) or
-            all results (True). Optional, defaults to False.
-    """
-    plist_path = '/usr/local/sal/plugin_results.plist'
-    if os.path.exists(plist_path):
-        plugin_results = FoundationPlist.readPlist(plist_path)
-    else:
-        plugin_results = []
-
-    plugin_results.append({'plugin': plugin, 'historical': historical, 'data': data})
-    FoundationPlist.writePlist(plugin_results, plist_path)
+    utils.add_plugin_results('ARD_Info', data)
 
 
 if __name__ == "__main__":
--- server/plugins/uptime/scripts/uptime.py
+++ server/plugins/uptime/scripts/uptime.py
@@ -7,15 +7,15 @@ import subprocess
 import sys
 import time
 
-sys.path.append('/usr/local/munki')
-from munkilib import FoundationPlist
+sys.path.append("/usr/local/sal")
+import utils
 
 
 def main():
     uptime_seconds = get_uptime()
     data = {'UptimeDays': uptime_seconds / 60 / 60 / 24,
             'UptimeSeconds': uptime_seconds}
-    add_plugin_results('Uptime', data)
+    utils.add_plugin_results('Uptime', data)
 
 
 def get_uptime():
@@ -26,28 +26,6 @@ def get_uptime():
     sec = int(re.sub(r'.*sec = (\d+),.*', '\\1', output))
     up = int(time.time() - sec)
     return up if up > 0 else -1
-
-
-def add_plugin_results(plugin, data, historical=False):
-    """Add data to the shared plugin results plist.
-
-    This function creates the shared results plist file if it does not
-    already exist; otherwise, it adds the entry by appending.
-
-    Args:
-        plugin (str): Name of the plugin returning data.
-        data (dict): Dictionary of results.
-        historical (bool): Whether to keep only one record (False) or
-            all results (True). Optional, defaults to False.
-    """
-    plist_path = '/usr/local/sal/plugin_results.plist'
-    if os.path.exists(plist_path):
-        plugin_results = FoundationPlist.readPlist(plist_path)
-    else:
-        plugin_results = []
-
-    plugin_results.append({'plugin': plugin, 'historical': historical, 'data': data})
-    FoundationPlist.writePlist(plugin_results, plist_path)
 
 
 if __name__ == '__main__':

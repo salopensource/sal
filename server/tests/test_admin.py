878532e65047817ac8ac3abb6fbf0241da0759c6
Shea Craig <sheagcraig@gmail.com>
2019-03-04 10:47:58 -0500

    Resolve test failure for removed models.

--- server/tests/test_admin.py
+++ server/tests/test_admin.py
@@ -7,7 +7,7 @@ from sal.test_utils import AdminTestCase
 class ServerAdminTest(AdminTestCase):
     """Test the admin site is configured to have all expected views."""
     admin_endpoints = {
-        'apikey', 'businessunit', 'fact', 'historicalfact', 'installedupdate',
-        'machinedetailplugin', 'machinegroup', 'machine', 'pendingappleupdate', 'pluginscriptrow',
-        'pluginscriptsubmission', 'plugin', 'report', 'salsetting', 'updatehistoryitem',
-        'updatehistory'}
+        'apikey', 'businessunit', 'fact', 'historicalfact',
+        'machinedetailplugin', 'machinegroup', 'machine', 'manageditem', 'managementsource',
+        'pluginscriptrow', 'pluginscriptsubmission', 'plugin', 'report', 'salsetting',
+        'updatehistoryitem', 'updatehistory'}

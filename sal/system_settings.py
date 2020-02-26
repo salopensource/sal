578f4d7084705a5a5ef4ab219e3959a80a0f6663
nathandarnell <nathanrdarnell@gmail.com>
2020-02-26 14:12:04 -0800

    Update system_settings.py

--- sal/system_settings.py
+++ sal/system_settings.py
@@ -101,7 +101,7 @@ ALLOWED_HOSTS = ['*']
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'Europe/London'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html

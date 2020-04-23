f6f431f9d17ffde89ce9fbcc1383e37091fcfe6b
Shea Craig <sheagcraig@gmail.com>
2020-04-23 10:52:10 -0400

    Move query filter generation inside plugin method to check every load.

--- server/plugins/activity/activity.py
+++ server/plugins/activity/activity.py
@@ -6,37 +6,39 @@ from django.db.models import Q
 import sal.plugin
 
 
-NOW = django.utils.timezone.now()
-HOUR_AGO = NOW - timedelta(hours=1)
-TODAY = NOW - timedelta(hours=24)
-MONTH_AGO = TODAY - timedelta(days=30)
-THREE_MONTHS_AGO = TODAY - timedelta(days=90)
-
-FILTERS_AND_TITLES = {
-    'hour': (Q(last_checkin__gte=HOUR_AGO), 'Machines seen in the last hour'),
-    'today': (Q(last_checkin__gte=TODAY), 'Machines seen today'),
-    'month': (Q(last_checkin__range=(THREE_MONTHS_AGO, MONTH_AGO)),
-              'Machines inactive for a month'),
-    'three_months': (Q(last_checkin__lte=THREE_MONTHS_AGO), 'Machines inactive for over 3 months')}
-
-
 class Activity(sal.plugin.Widget):
 
     widget_width = 12
     description = 'Current Munki activity'
 
+    def _get_q_and_title(self, data):
+        now = django.utils.timezone.now()
+        today = now - timedelta(hours=24)
+        month_ago = today - timedelta(days=30)
+        three_months_ago = today - timedelta(days=90)
+        if data == 'hour':
+            hour_ago = now - timedelta(hours=1)
+            return Q(last_checkin__gte=hour_ago), 'Machines seen in the last hour'
+        elif data == 'today':
+            return Q(last_checkin__gte=today), 'Machines seen today'
+        elif data == 'month':
+            return (
+                Q(last_checkin__range=(three_months_ago, month_ago)),
+                'Machines inactive for a month')
+        elif data == 'three_months':
+            return Q(last_checkin__lte=three_months_ago), 'Machines inactive for over 3 months'
+        else:
+            return None, None
+
     def get_context(self, queryset, **kwargs):
         context = self.super_get_context(queryset, **kwargs)
 
-        for key in FILTERS_AND_TITLES:
+        for key in ('hour', 'today', 'month', 'three_months'):
             filtered_machines, _ = self.filter(queryset, key)
             context[key] = filtered_machines.count()
 
         return context
 
     def filter(self, queryset, data):
-        try:
-            time_filter, title = FILTERS_AND_TITLES[data]
-        except KeyError:
-            return None, None
+        time_filter, title = self._get_q_and_title(data)
         return queryset.filter(time_filter), title

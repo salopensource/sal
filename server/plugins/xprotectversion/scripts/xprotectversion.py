963d7953467e9837d11b655eee00a28cc0780eef
Michael Krause <krause@mpib-berlin.mpg.de>
2022-04-06 14:15:36 +0200

    Plugins: fix reading Xprotect version on newer macOS version

--- server/plugins/xprotectversion/scripts/xprotectversion.py
+++ server/plugins/xprotectversion/scripts/xprotectversion.py
@@ -3,21 +3,34 @@
 
 import pathlib
 import plistlib
+import os
 
 import sal
 
 
-def main():
-    plist_path = pathlib.Path(
-        '/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/XProtect.meta.plist')
+
+def xprotect_version():
+    try:
+        darwin_ver = int(os.uname().release.split('.')[0])
+    except:
+        return 'Not supported'
+    if darwin_ver >= 20:  # Big Sur 11.x
+        xprotect = '/Library/Apple/System/Library/CoreServices/XProtect.bundle/Contents/Info.plist'
+        key = 'CFBundleShortVersionString'
+    else:
+        xprotect = '/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/XProtect.meta.plist'
+        key = 'Version'
+    plist_path = pathlib.Path(xprotect)
     if plist_path.exists():
         plist = plistlib.loads(plist_path.read_bytes())
-        version = str(plist['Version'])
+        return str(int(plist[key]))
     else:
-        version = 'Not supported'
+        return 'Not supported'
 
+
+def main():
+    version = xprotect_version()
     sal.add_plugin_results('XprotectVersion', {'Version': version})
-
 
 if __name__ == '__main__':
     main()

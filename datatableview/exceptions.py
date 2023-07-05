e5e87304068b17a133a2612ac4e9802f02908bf2
Shea Craig <sheagcraig@users.noreply.github.com>
2023-07-05 12:00:00 -0400

    Delete vendored django datatableview and use pypi version 2.1.6

--- datatableview/exceptions.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-
-class ColumnError(Exception):
-    """ Some kind of problem with a datatable column. """
-
-
-class SkipRecord(Exception):
-    """ User-raised problem with a record during serialization. """
--- datatableview/views/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from .base import *
-from .xeditable import *

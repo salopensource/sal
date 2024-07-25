81ac9ab89630de8ada9a4d093e3ee50d854345f4
Austin Culter <austin.culter@faire.com>
2024-07-25 10:36:25 +0200

    Implement django-iam-dbauth logic

--- docker/settings.py
+++ docker/settings.py
@@ -31,19 +31,6 @@ if 'MEMCACHED_PORT_11211_TCP_ADDR' in os.environ:
 host = None
 port = None
 
-if 'DB_USER' in os.environ:
-    if 'DB_HOST' in os.environ:
-        host = os.environ.get('DB_HOST')
-        port = os.environ.get('DB_PORT', '5432')
-
-    elif 'DB_PORT_5432_TCP_ADDR' in os.environ:
-        host = os.environ.get('DB_PORT_5432_TCP_ADDR')
-        port = os.environ.get('DB_PORT_5432_TCP_PORT', '5432')
-
-    else:
-        host = 'db'
-        port = '5432'
-
 if host and port:
     DATABASES = {
         'default': {
@@ -53,5 +40,35 @@ if host and port:
             'PASSWORD': os.environ['DB_PASS'],
             'HOST': host,
             'PORT': port,
+        }
+    }
+
+if 'AWS_IAM' in os.environ:
+    import requests
+    cert_bundle_url = 'https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem'
+    cert_target_path = '/etc/ssl/certs/global-bundle.pem'
+
+    response = requests.get(cert_bundle_url)
+    if response.status_code == 200:
+        os.makedirs(os.path.dirname(cert_target_path), exist_ok=True)
+
+        with open(cert_target_path, 'wb') as file:
+            file.write(response.content)
+        print(f"AWS RDS cert bundle successfully downloaded and saved to {cert_target_path}")
+    else:
+        print(f"Failed to download AWS RDS cert bundle, status code: {response.status_code}")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django_iam_dbauth.aws.postgresql',
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'HOST': os.environ['DB_HOST'],
+            'PORT': os.environ['DB_PORT'],
+            'OPTIONS' : {
+                'region_name': os.environ['AWS_RDS_REGION'],
+                'sslmode': 'verify-full',
+                'sslrootcert': '/etc/ssl/certs/global-bundle.pem',
+                'use_iam_auth': True,
+            }
         }
     }
